aa1eec621dc472a5faed1e9feab22e678e1b33c0
Suat Özgür <s.oezguer@gmail.com>
2021-04-02 11:54:11 +0200

    wip

--- controller/Test.php
+++ controller/Test.php
@@ -5,28 +5,48 @@ class Test extends AbstractController {
     protected $_template = 'test';
 
     public function action() {
-        $sql = "SELECT * FROM hospitalisierungen ORDER BY date asc";
+        $showNumWeeks = 10;
+        $ewz = 1899160;
+        $week = 0;
+        $weekD = [];
+        $day = 1;
+        $data = [];
+        $data[0] = [];
 
-        foreach ($this->_pdo->query($sql, PDO::FETCH_ASSOC) as $row) {
+        $dates = [];
+        $values = [];
+        $sevenDay = [0, 0, 0, 0, 0, 0, 0];
+
+        $vaxxed = 0;
+        $sql = "SELECT * FROM cases WHERE date > '2021-01-01' ORDER BY date asc";
+        foreach ($this->_pdo->query($sql) as $row) {
             $dates[] = $row['date'];
-            $next = 0;
-            $sum = 0;
-
-            foreach (array_keys($row) as $columnName) {
-
-                if ($columnName != 'date') {
-                    $values[$next]['label'] = $columnName;
-                    $values[$next]['values'][] = $row[$columnName];
-                    $values[$next]['totalValues'][] = $row[$columnName];
-                    $sum += $row[$columnName];
-                    $next++;
-                }
-            }
-
+            array_shift($sevenDay);
+            array_push($sevenDay, $row['cases']);
+            $vaxxed += $row['vaccination-2nd'];
+            $values[] = round(100000 / $ewz * array_sum($sevenDay), 2);
+            $values2[] = round(100000 / ($ewz-$vaxxed) * array_sum($sevenDay), 2);
         }
 
+        $sql = "SELECT * FROM cases ORDER BY date desc";
+        foreach ($this->_pdo->query($sql) as $row) {
+
+            $data[$week][7 - $day] = $row['cases'];
+            $day++;
+            if ($day > 7) {
+                $weekD[$week] = $row['date'];
+                $week++;
+                $day = 1;
+            }
+        }
+
+        $this->assign('showNumWeeks', $showNumWeeks);
+        $this->assign('data', $data);
         $this->assign('dates', $dates);
         $this->assign('values', $values);
-
+        $this->assign('values2', $values2);
+        $this->assign('weekD', $weekD);
+        $this->assign('ewz', $ewz);
     }
+
 }
