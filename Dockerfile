2ecbc99ddeb14a742cd953d6128f208db34b8631
Suat Özgür <s.oezguer@gmail.com>
2022-10-21 00:50:10 +0200

    cleanup + prepare for docker

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,30 @@
-FROM nginx:1.21.6
-WORKDIR /app
+FROM trafex/php-nginx
+
+USER root
+RUN apk add --no-cache \
+    php81-pecl-memcache \
+    php81-pdo \
+    php81-pdo_mysql \
+    memcached
+
+
+
+RUN mkdir /etc/nginx/conf.d
+COPY ./config/nginx.conf /etc/nginx/conf.d/nginx.conf
+
+WORKDIR /var/www
 COPY . ./
-COPY ./config/nginx.conf /etc/nginx/conf.d/default.conf
+
+
+RUN echo "[program:memcached]" >> /etc/supervisor/conf.d/supervisord.conf
+RUN echo "command=memcached -u memcached" >> /etc/supervisor/conf.d/supervisord.conf
+RUN echo "stdout_logfile=/dev/stdout" >> /etc/supervisor/conf.d/supervisord.conf
+RUN echo "stdout_logfile_maxbytes=0" >> /etc/supervisor/conf.d/supervisord.conf
+RUN echo "stderr_logfile=/dev/stderr" >> /etc/supervisor/conf.d/supervisord.conf
+RUN echo "stderr_logfile_maxbytes=0" >> /etc/supervisor/conf.d/supervisord.conf
+RUN echo "autorestart=false" >> /etc/supervisor/conf.d/supervisord.conf
+RUN echo "startretries=0" >> /etc/supervisor/conf.d/supervisord.conf
+
+#COPY ./config/nginx.conf /etc/nginx/conf.d/default.conf
+
+USER nobody
--- htdocs/index.php
+++ htdocs/index.php
@@ -23,18 +23,19 @@ if (!isset($tmp[1]) || $tmp[1] == '') { $page = 'neuinfektionen2022daily'; } els
 
 if (file_exists(dirname(__FILE__). '/../controller/' . ucfirst($page) . '.php')) {
 
-    $memcache = new Memcached;
-    $memcache->addServer('localhost', 11211);
+    $memcache = new Memcache;
+    $memcache->addServer('127.0.0.1', 11211);
     $key = md5(json_encode(['7' . $_SERVER['SERVER_NAME'], $_GET, $_SERVER['REQUEST_URI'], json_encode($_GET)]));
     $content = $memcache->get($key);
 
-
-    $cacheEnabled = ($config['settings']['caching']) && $page != 'kontakt';
+    $cacheEnabled = ($config['settings']['caching']);
 
     if ($cacheEnabled && $content = $memcache->get($key)) {
         header('X-Cache: ' . $key);
         echo $content;
     } else {
+        header('X-No-Cache: ' . $key);
+
         ob_start();
         $page = ucfirst($page);
 
@@ -44,7 +45,7 @@ if (file_exists(dirname(__FILE__). '/../controller/' . ucfirst($page) . '.php'))
         $content = ob_get_contents();
         ob_end_clean();
         echo $content;
-        $memcache->add($key, $content, 60*60);
+        echo $memcache->add($key, $content, null, 60*60);
     }
 } else {
     $c = new ErrorController($tmp);
